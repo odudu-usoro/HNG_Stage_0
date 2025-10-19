0333495dba9b7ad6b48b34e6da363a91dac52eb6
Ben-153 <benakoja.10@gmail.com>
2025-10-19 13:12:10 +0100

    Prepare Django app for Railway deployment

--- profile_api/settings.py
+++ profile_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$_t!5y!n%%5q+ic*@f+i5-39_k)kalvq0&yeq26vk-4lrfjh$('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
